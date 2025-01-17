da13779def897edb0f3e363334fe28406014c3dc
Kay <rn.kay99@icloud.com>
2025-01-17 23:10:02 +0100

    Initial Commit

---
+++ FitnessAI/FitnessAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAIApp.swift
+//  FitnessAI
+//
+//  Created by Kay Steinbauer on 17.01.25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAITests/FitnessAITests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAITests.swift
+//  FitnessAITests
+//
+//  Created by Kay Steinbauer on 17.01.25.
+//
+
+import Testing
+@testable import FitnessAI
+
+struct FitnessAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
